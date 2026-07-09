8d4435348530b3dbd495a08e07462f9bcf1295aa
Jerry Marollano <70122455+chrmux@users.noreply.github.com>
2026-07-09 20:18:05 +0800

    Delete server/routes directory

--- server/routes/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/professional', require('./professional'));
-
-module.exports = router;
--- server/routes/route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const controller = require("../controllers/controller");
-
-router.route("/").get(controller.landing);
-
-module.exports = router;
